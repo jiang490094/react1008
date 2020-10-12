7d1767a74208c91fadcc36d96b04bc3dc2d5db95
jiang490094 <jfr08100810@gmail.com>
2020-10-12 11:11:48 +0800

    解構賦值與函式型元件與類別型元件

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      total: 0,
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <h1>{this.state.total}</h1>
+        <button onClick={() => this.setState({ total: this.state.total + 1 })}>
+          +1
+        </button>
+        <button onClick={() => this.setState({ total: this.state.total - 1 })}>
+          -1
+        </button>
+      </>
+    )
+  }
 }
 
-export default App;
+export default App
+
+// // 導入react函式庫
+// import React from 'react'
+
+// // 繼承類別
+// class App extends React.Component {
+//   // 建構式
+//   constructor() {
+//     //建構式的第一行需要使用super呼叫父母類別的建構式
+//     super()
+//     // 這裡建立
+//     this.state = {
+//       total: 0,
+//     }
+//   }
+
+//   // render的回傳值即為最後呈現在網頁上的元素
+//   render() {
+//     return (
+//       <>
+//         <h1>{this.state.total}</h1>
+//         <button onClick={() => this.setState({ total: this.state.total + 1 })}>
+//           +1
+//         </button>
+//         <button onClick={() => this.setState({ total: this.state.total - 1 })}>
+//           -1
+//         </button>
+//       </>
+//     )
+//   }
+// }
+
+// export default App
---
+++ src/AppFunction.js
@@ -0,0 +1,24 @@
+// 導入其它的模組
+import React, { useState } from 'react'
+
+function App() {
+  // 呼叫useState勾子，設定初始化值為0，total跟setTotal為解構賦值，各接一個值
+  //解構賦值是減寫法，專為物件跟陣列設計ex.(原本)const arr = [1,2].let a = arr[0].let b = arr[1]。(解構賦值版)const [a,b]=arr
+  // 回傳一組getter(獲取值)和setter(設定值)陣列
+  //useState是鉤子就像函式
+  const [total, setTotal] = useState(0)
+
+  // return 值即元件的 render 方法，只能有一個根元素進行render
+  // 多個元件需要用<>...</>(React.Fragment)包住
+  return (
+    <>
+      {/* JSX語法中加註解 */}
+      <h1>{total}</h1>
+      <button onClick={() => setTotal(total + 1)}>+1</button>
+      <button onClick={() => setTotal(total - 1)}>-1</button>
+    </>
+  )
+}
+
+// 輸出元件(函式)
+export default App
