c8c95f0b7bfb0dc75d1e8c6715685b687c81a29a
jiang490094 <jfr08100810@gmail.com>
2020-10-19 11:54:20 +0800

    1019上午

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,11 +1,27 @@
 import React, { useState, useEffect } from 'react'
+import { withRouter, useHistory } from 'react-router-dom'
 
 function NotFoundPage(props) {
   return (
     <>
       <h1>頁面不存在</h1>
+      <button
+        onClick={() => {
+          props.history.push('/product')
+        }}
+      >
+        到產品頁
+      </button>
+      <hr />
+      <button
+        onClick={() => {
+          props.history.goBack()
+        }}
+      >
+        到上一頁
+      </button>
     </>
   )
 }
 
-export default NotFoundPage
+export default withRouter(NotFoundPage)
