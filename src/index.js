078a2d35b6f885a2a1f21f3855a49972a1e37b38
jiang490094 <jfr08100810@gmail.com>
2020-10-20 15:47:12 +0800

    1020狀態容器

--- src/index.js
+++ src/index.js
@@ -7,6 +7,33 @@ import * as serviceWorker from './serviceWorker'
 // 第一步：匯入createStore, combineReducers API
 import { createStore, combineReducers } from 'redux'
 
+// 第二步：寫出reducer
+// action = { type: 'ADD_VALUE', value: 1 }
+// action = { type: 'MINUS_VALUE', value: 1 }
+//state初始值，與此Reducer接下來要做的action，整個像是個公式
+function counter(state = 0, action) {
+  switch (action.type) {
+    case 'ADD_VALUE':
+      return state + action.value
+    case 'MINUS_VALUE':
+      return state - action.value
+    default:
+      return state
+  }
+}
+
+// 第二之一：合併所有的reducers成一個大的reducer
+const rootReducer = combineReducers({
+  counter,
+})
+
+// 第三步：由rootReducer建立store
+//store是狀態容器
+const store = createStore(
+  rootReducer /* preloadedState, */,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
