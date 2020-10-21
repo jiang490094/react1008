6118f16fb094ed4308109b3e80282675eab79d71
jiang490094 <jfr08100810@gmail.com>
2020-10-21 15:59:10 +0800

    結束惹(´;ω;`)ヾ(･∀･`)(´;ω;`)ヾ(･∀･`)(´;ω;`)ヾ(･∀･`)

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,10 +6,38 @@ export const addValue = (value) => {
 }
 
 export const addValueAsync = (value) => {
-  return (dispatch) => {
-    setTimeout(() => {
+  return async function updateTotalToServer(dispatch, getState) {
+    // 開啟載入的指示圖示
+    //setDataLoading(true)
+
+    // https://redux.js.org/api/store#getstate
+    console.log(getState())
+
+    const newTotal = { total: getState().counter + value }
+
+    const url = 'http://localhost:5555/counter/1'
+
+    const request = new Request(url, {
+      method: 'PUT',
+      body: JSON.stringify(newTotal),
+      headers: new Headers({
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      }),
+    })
+
+    try {
+      const response = await fetch(request)
+      const data = await response.json()
+      // data會是一個物件值
+      console.log(data)
+
+      // 驗証成功後再設定…
+      //setTotal(total + value)
       dispatch(addValue(value))
-    }, 2000)
+    } catch (error) {
+      //setError(error)
+    }
   }
 }
 
